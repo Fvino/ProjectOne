47096d99f3723adaed5fffe4d991ce0e2813bc40
ValeriyFomin <fvg2@icloud.com>
2022-04-27 12:46:48 +0200

    Initial Commit

---
+++ ProjectOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectOne
+//
+//  Created by Valeriy Fomin on 27/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
