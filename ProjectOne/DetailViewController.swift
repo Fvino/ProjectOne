4397d88bc3364ae2d8bd7c12eb2d4dc90c3251a9
ValeriyFomin <fvg2@icloud.com>
2022-04-28 15:53:19 +0200

    add hidesBarsOnTap, bigTitle

--- ProjectOne/DetailViewController.swift
+++ ProjectOne/DetailViewController.swift
@@ -19,8 +19,21 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
+
         if let imageToLoad = selectedImage {
             imageViewOutlet.image = UIImage(named: imageToLoad)
         }
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
     }
 }
