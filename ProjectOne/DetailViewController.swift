bb741ce6bc0b4f9e4eb6a2534d00e4bd9e940e16
ValeriyFomin <fvg2@icloud.com>
2022-04-28 11:53:42 +0200

    add detailViewController, add imageView

---
+++ ProjectOne/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  ProjectOne
+//
+//  Created by Valeriy Fomin on 28/4/22.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    //MARK: - Outlets
+    @IBOutlet weak var imageViewOutlet: UIImageView!
+
+    //MARK: - ViewController
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
